6ef682143e98a8ea2e7647c1a0da42bdd14a0cd6
gbw767 <31829675+gbw767@users.noreply.github.com>
2019-07-10 20:03:09 -0230

    working and revised

--- src/com/company/GameFrame.java
+++ src/com/company/GameFrame.java
@@ -5,37 +5,37 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 class GameFrame extends JFrame {
 
-    private Random randy = new Random();
     private ArrayList<JToggleButton> bList = new ArrayList<>();
-    private ArrayList<JToggleButton> bList2 = new ArrayList<>();
     private boolean oneMatch = false;
-    private ActionListener listener;
+    private Object flag;
 
     GameFrame() {
-                GridLayout layout = new GridLayout();
+        GridLayout layout = new GridLayout();
         layout.setColumns(2);
         layout.setRows(2);
         setLayout(layout);
 
         final String[] selected = {null};
 
-        buttonSetUp("lizard.png", bList, bList2);
-        buttonSetUp("bird.png", bList, bList2);
+
+        buttonSetUp("lizard.png", bList);
+        buttonSetUp("bird.png", bList);
+
 
         boredsetUp();
 
-        listener = new ActionListener() {
-            Object flag;
+        ActionListener listener = new ActionListener() {
+
 
             public void actionPerformed(ActionEvent e) {
                 if (selected[0] == null) {
                     selected[0] = e.getActionCommand();
                     flag = e.getSource();
-
                     System.out.println(e.getActionCommand());
                 } else if (e.getSource() == flag) {
                     flag = null;
@@ -46,31 +46,24 @@ class GameFrame extends JFrame {
                     remove((Component) e.getSource());
                     remove((Component) flag);
                     repaint();
-                    if (oneMatch){
-                        JOptionPane.showMessageDialog(null,"ya did it");
+                    if (oneMatch) {
+                        JOptionPane.showMessageDialog(null, "ya did it");
                         boredsetUp();
 
-                        oneMatch ^=true;
-                    }else oneMatch ^=true;
-
-
-
-
-
+                        oneMatch ^= true;
+                    } else oneMatch = true;
                     flag = null;
 
-                } else selected[0] = null;
-                System.out.println("Button selected: " + e.getActionCommand());
-
-
+                } else {
+                    selected[0] = null;
+                    JOptionPane.showMessageDialog(null, "wrong one dumb dumb");
+                }
             }
         };
+        for (JToggleButton button : bList) {
+            button.addActionListener(listener);
 
-        for (int i = 0; i < bList.size(); i++) {
-            bList.get(i).addActionListener(listener);
-            bList2.get(i).addActionListener(listener);
         }
-
     }
 
     private ImageIcon resizeImage(String imagePath) {
@@ -80,29 +73,20 @@ class GameFrame extends JFrame {
     }
 
     private void buttonSetUp(String s,
-                             ArrayList<JToggleButton> al,
-                             ArrayList<JToggleButton> al2) {
-        JToggleButton button;
-        JToggleButton button2;
-        Icon i = resizeImage("images\\" + s);
-        al.add(button = new JToggleButton(i));
-        al2.add(button2 = new JToggleButton(i));
-        button2.setActionCommand(s);
-        button.setActionCommand(s);
-
-
+                             ArrayList<JToggleButton> al) {
+        for (int i = 0; i < 2; i++) {
+            JToggleButton button;
+            Icon con = resizeImage("images\\" + s);
+            al.add(button = new JToggleButton(con));
+            button.setActionCommand(s);
+        }
     }
-    private void boredsetUp(){
-        int rand = randy.nextInt(2);
-        int dinner;
-        do {
-            dinner = randy.nextInt(2);
-        }while (rand == dinner);
-        add(bList.get(rand));
-        add(bList2.get(rand));
 
-        add(bList.get(dinner));
-        add(bList2.get(dinner));
+    private void boredsetUp() {
+        Collections.shuffle(bList);
+        for (JToggleButton button : bList) {
+            add(button);
+        }
         repaint();
         pack();
     }
