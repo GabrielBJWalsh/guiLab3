ed8a2437cc0e874dd5b145670bce4770798d833b
gabrielB.walsh <g.b.j.walsh@gmail.com>
2019-07-10 10:52:05 -0230

    needs bugFixes

--- src/com/company/GameFrame.java
+++ src/com/company/GameFrame.java
@@ -29,20 +29,21 @@ class GameFrame extends JFrame {
         ArrayList<JToggleButton> bList = new ArrayList<>();
         ArrayList<JToggleButton> bList2 = new ArrayList<>();
 
-        //buttonSetUp(pic0, pic0, "cat.png", bList, bList2);
-        //buttonSetUp(pic1, pic1, "dog.png", bList, bList2);
-        //buttonSetUp(pic2, pic2, "lizard.png", bList, bList2);
+        buttonSetUp(pic0, pic0, "cat.png", bList, bList2);
+        buttonSetUp(pic1, pic1, "dog.png", bList, bList2);
+        buttonSetUp(pic2, pic2, "lizard.png", bList, bList2);
         buttonSetUp(pic3, pic3, "bird.png", bList, bList2);
         buttonSetUp(pic4, pic4, "hamster.png", bList, bList2);
         buttonSetUp(pic5, pic5, "turtle.png", bList, bList2);
 
-        Collections.shuffle(bList);
-        Collections.shuffle(bList2);
+        //Collections.shuffle(bList);
+        //Collections.shuffle(bList2);
 
         add(bList.get(0));
         add(bList2.get(0));
         add(bList.get(1));
         add(bList2.get(1));
+        pack();
 
         ActionListener listener = new ActionListener() {
             Object flag;
@@ -58,14 +59,14 @@ class GameFrame extends JFrame {
                     System.out.println("match");
                     remove((Component) e.getSource());
                     remove((Component) flag);
-                    flag = null;
-                    try {
-                        add(bList.get(2));
-                        add(bList2.get(2));
-                    } catch (Exception e2) {
-                        System.out.println(e2.getMessage());
-                    }
+                    //repaint();4
+
+                    add(bList.get(randy.nextInt(5)));
+                    add(bList2.get(randy.nextInt(5)));
+                    pack();
+
                     repaint();
+                    flag = null;
 
                 } else selected[0] = null;
                 System.out.println("Button selected: " + e.getActionCommand());
@@ -75,19 +76,23 @@ class GameFrame extends JFrame {
         };
 
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             bList.get(i).addActionListener(listener);
             bList2.get(i).addActionListener(listener);
         }
 
     }
+    private ImageIcon resizeImage(String imagePath){
+        ImageIcon icon = new ImageIcon(getClass().getResource(imagePath));
+        Image scaledIcon = icon.getImage().getScaledInstance(225,300, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledIcon);}
 
     private void buttonSetUp(JToggleButton button,
                              JToggleButton button2,
                              String s,
                              ArrayList<JToggleButton> al,
                              ArrayList<JToggleButton> al2) {
-        Icon i = new ImageIcon(getClass().getResource(("images\\" + s)));
+        Icon i = resizeImage("images\\"+s);
         al.add(button = new JToggleButton(i));
         al2.add(button2 = new JToggleButton(i));
         button2.setActionCommand(s);
